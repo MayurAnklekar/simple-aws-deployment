1e37f253809ec4011db0b5eaf6d636f2891de9d3
MayurAnklekar <mayuranklekar1010@gmail.com>
2022-01-27 23:55:03 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello devops
+          Learn devops v2
         </p>
         <a
           className="App-link"
